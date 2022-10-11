e62c6dba8ca12db3c41561ed5d99e720e20fb471
Akashj Pro <hatm-itsj@hikesiya.co.jp>
2022-10-11 13:09:07 +0700

    Init Project

---
+++ PlayVideoCleanArchitecture/PlayVideoCleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayVideoCleanArchitectureApp.swift
+//  PlayVideoCleanArchitecture
+//
+//  Created by A Tang Ky on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PlayVideoCleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
