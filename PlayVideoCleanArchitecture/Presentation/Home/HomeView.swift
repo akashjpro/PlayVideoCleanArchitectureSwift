c8dcf54ef694ad6dc028b977b4768c7ad587c71f
Akashj Pro <hatm-itsj@hikesiya.co.jp>
2022-10-11 13:36:42 +0700

    Update Home

---
+++ PlayVideoCleanArchitecture/Presentation/Home/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  PlayVideoCleanArchitecture
+//
+//  Created by A Tang Ky on 10/11/22.
+//
+
+import SwiftUI
+
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
